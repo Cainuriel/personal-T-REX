f8eaab918a490c8319c165bd9b7dde8cecb30a3b
Fernando Lopez <cainuriel@gmail.com>
2025-06-16 15:20:07 +0200

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -29,19 +29,9 @@ const config: HardhatUserConfig = {
     debugMode: true,
     outputDir: "./docgen",
     freshOutput: true,
-  },  networks: {
-   
-    taycan: {
-      url: "http://5.250.188.118:8545",
-      accounts: [
-        process.env.ADMIN_WALLET_PRIV_KEY,
-        process.env.INVESTOR1_PRIV_KEY,
-        process.env.INVESTOR2_PRIV_KEY
-      ].filter((key): key is string => !!key),
-      timeout: 300000, 
-    },
-       
-  },
+  },  
+  
+  
 };
 
 export default config;
